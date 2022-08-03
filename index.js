2e11eed2eefb96029dabbc672325b5a63e135c80
Mike Pacific <mike.pacific@gmail.com>
2022-08-02 23:19:39 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const MusicMetadata = require('music-metadata')
+const SanitizeFilename = require('sanitize-filename')
+const ReplaceSpecialCharacters = require('replace-special-characters')
+const FS = require('fs').promises
+const path = require('path')
+
+// Change sourcePath to where your original Music folder lives
+const sourcePath = '/Users/mike/Dropbox/Music'
+const destPath = './Music'
+
+const getFileInfo = async (filePath, ignoreCompilation) => {
+  const fileMetadata = await MusicMetadata.parseFile(filePath)
+  const fileInfo = await path.parse(filePath)
+  const album = fileMetadata.common.album?.trim() || 'Unknown Album'
+  const track = fileMetadata.common.track?.no
+  let artist = fileMetadata.common.albumartist?.trim() || fileMetadata.common.artists?.join(', ') || fileMetadata.common.artist?.trim() || 'Unknown Artist'
+  let title = fileMetadata.common.title?.trim() || fileInfo.name
+
+  if (ignoreCompilation) {
+    artist = fileMetadata.common.artists?.join(', ') || fileMetadata.common.artist?.trim() || 'Unknown Artist'
+  }
+
+  if (fileMetadata.common.compilation && !ignoreCompilation) {
+    title = `${fileMetadata.common.artists?.join(', ') || artist} - ${title}`
+    artist = 'Various Artists'
+  }
+
+  return { album, track, artist, title, fileInfoName: fileInfo.name, fileInfoExt: fileInfo.ext }
+}
+
+const processFile = async (filePath, ignoreCompilation = false) => {
+  // console.log(`File: ${filePath}`)
+  try {
+    const { album, track, artist, title, fileInfoName, fileInfoExt } = await getFileInfo(filePath, ignoreCompilation)
+
+    const artistFolderName = ReplaceSpecialCharacters(SanitizeFilename(artist, { replacement: '_' }))
+    const albumFolderName = ReplaceSpecialCharacters(SanitizeFilename(album, { replacement: '_' }))
+    const fileName = `${track ? `${track}. ` : ''}${ReplaceSpecialCharacters(SanitizeFilename(title || fileInfoName))}${fileInfoExt}`
+
+    const artistFolderPath = `${destPath}/${artistFolderName}`
+    const albumFolderPath = `${artistFolderPath}/${albumFolderName}`
+
+    try {
+      await FS.access(artistFolderPath)
+    } catch (error) {
+      await FS.mkdir(artistFolderPath)
+    }
+    try {
+      await FS.access(albumFolderPath)
+    } catch (error) {
+      await FS.mkdir(albumFolderPath)
+    }
+
+    await FS.copyFile(filePath, `${albumFolderPath}/${fileName}`)
+  } catch (error) {
+    console.log(`Error (${filePath}): ${error}`, error)
+  }
+}
+
+const followPath = async (sourcePath) => {
+  // console.log(`Directory: ${sourcePath}`);
+
+  const directoryContents = await FS.readdir(sourcePath, { withFileTypes: true })
+  for (const item of directoryContents) {
+    const ignoreItems = ['.DS_Store', '.localized', '.Media Preferences.plist']
+    if (ignoreItems.some(ignoreItem => ignoreItem === item.name)) continue
+
+    const itemPath = `${sourcePath}/${item.name}`
+    if (item.isDirectory()) {
+      await followPath(itemPath)
+    } else {
+      await processFile(itemPath)
+    }
+  }
+}
+
+const checkCompilation = async compilationPath => {
+  const directoryContents = await FS.readdir(compilationPath, { withFileTypes: true })
+  const artists = []
+  for (const item of directoryContents) {
+    const itemPath = `${compilationPath}/${item.name}`
+    const { artist } = await getFileInfo(itemPath, true)
+
+    if (!artists.some(storedArtist => storedArtist === artist)) {
+      artists.push(artist)
+    }
+  }
+  if (artists.length === 1 && artists[0] !== 'Various Artists') {
+    for (const item of directoryContents) {
+      const itemPath = `${compilationPath}/${item.name}`
+      await processFile(itemPath, true)
+    }
+    await FS.rm(compilationPath, { recursive: true })
+  }
+}
+
+const checkCompilations = async compilationsPath => {
+  const directoryContents = await FS.readdir(compilationsPath, { withFileTypes: true })
+  for (const item of directoryContents) {
+    if (item.isDirectory()) {
+      const itemPath = `${compilationsPath}/${item.name}`
+      await checkCompilation(itemPath)
+    }
+  }
+}
+
+const doScript = async () => {
+  await followPath(sourcePath)
+  await checkCompilations(`${destPath}/Various Artists`)
+}
+
+doScript()
