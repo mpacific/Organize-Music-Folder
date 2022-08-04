ffdfe46237ef468dcf79480c03adc55dc597c834
Mike Pacific <mike.pacific@gmail.com>
2022-08-04 13:47:21 -0700

    Begin work on iTunes library (XML) work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const MusicMetadata = require('music-metadata')
 const SanitizeFilename = require('sanitize-filename')
 const ReplaceSpecialCharacters = require('replace-special-characters')
+const PList = require('plist')
 const FS = require('fs').promises
 const path = require('path')
 
@@ -55,6 +56,8 @@ const processFile = async (filePath, ignoreCompilation = false) => {
 
     console.log(`-> Copying to ${albumFolderPath}/${fileName}`)
     await FS.copyFile(filePath, `${albumFolderPath}/${fileName}`)
+    const oldFileStat = await FS.stat(filePath)
+    await FS.utimes(`${albumFolderPath}/${fileName}`, oldFileStat.atime, oldFileStat.mtime)
     console.log(`-> Copied to ${albumFolderPath}/${fileName}`)
   } catch (error) {
     console.log(`Error (${filePath}): ${error}`, error)
@@ -108,9 +111,62 @@ const checkCompilations = async compilationsPath => {
   }
 }
 
+const updateTrackLocations = async tracks => {
+  for (const track of tracks) {
+    track.Location = track.Location.replace(process.env.ITUNES_LIBRARY_OLD_MUSIC_PATH, process.env.SOURCE_PATH)
+  }
+}
+
+const processTrackSegments = async (playlists, tracks) => {
+  const playlist = playlists.find(playlist => playlist.Name === process.env.ITUNES_LIBRARY_SEGMENT_PLAYLIST)
+  if (!playlist) {
+    return {
+      All: tracks
+    }
+  }
+  const segmentedFolder = process.env.ITUNES_LIBRARY_SEGMENT_PLAYLIST_FOLDER || process.env.ITUNES_LIBRARY_SEGMENT_PLAYLIST
+  const unsegmentedFolder = process.env.ITUNES_LIBRARY_UNSEGMENTED_FOLDER || 'Other'
+  const segmentedTracks = {
+    [segmentedFolder]: [],
+    [unsegmentedFolder]: []
+  }
+  const playlistTracks = playlist['Playlist Items'].map(item => item['Track ID'])
+
+  for (const track of tracks) {
+    if (playlistTracks.some(playlistTrackID => playlistTrackID === track['Track ID'])) {
+      segmentedTracks[segmentedFolder].push(track)
+    } else {
+      segmentedTracks[unsegmentedFolder].push(track)
+    }
+  }
+
+  return segmentedTracks
+}
+
+const processiTunesLibrary = async () => {
+  const iTunesLibraryFileData = await FS.readFile(process.env.ITUNES_LIBRARY_PATH)
+  const iTunesLibraryData = PList.parse(iTunesLibraryFileData.toString())
+  let tracks = Object.values(iTunesLibraryData.Tracks).filter(track => track.Location)
+  if (process.env.ITUNES_LIBRARY_OLD_MUSIC_PATH) {
+    await updateTrackLocations(tracks)
+  }
+  if (process.env.ITUNES_LIBRARY_SEGMENT_PLAYLIST) {
+    tracks = await processTrackSegments(iTunesLibraryData.Playlists, tracks)
+  } else {
+    tracks = {
+      All: tracks
+    }
+  }
+  console.log(tracks)
+}
+
 const doScript = async () => {
-  await followPath(sourcePath)
-  await checkCompilations(`${destPath}/Various Artists`)
+  if (process.env.ITUNES_LIBRARY_PATH) {
+    await processiTunesLibrary()
+  } else {
+    await followPath(sourcePath)
+    await checkCompilations(`${destPath}/Various Artists`)
+  }
 }
 
 doScript()
