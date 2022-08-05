f3708e5186d3b27b1b90354dda7dc4fa15369366
Mike Pacific <mike.pacific@gmail.com>
2022-08-04 17:25:34 -0700

    Small tidying up

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const MusicFolder = require("./classes/class.musicFolder.js");
 const iTunesLibrary = require("./classes/class.iTunes.js");
 
 const initScript = async () => {
+  let OrganizeStrategy;
   if (process.env.ITUNES_LIBRARY_PATH) {
     // eslint-disable-next-line new-cap
-    const OrganizeiTunesFolder = new iTunesLibrary(
+    OrganizeStrategy = new iTunesLibrary(
       process.env.SOURCE_PATH,
       process.env.DEST_PATH
     );
-    await OrganizeiTunesFolder.buildTrackMap();
-    await OrganizeiTunesFolder.processTracks();
   } else {
-    const OrganizeMusicFolder = new MusicFolder(
+    OrganizeStrategy = new MusicFolder(
       process.env.SOURCE_PATH,
       process.env.DEST_PATH
     );
-    await OrganizeMusicFolder.buildTrackMap();
-    await OrganizeMusicFolder.processTracks();
   }
+  await OrganizeStrategy.buildTrackMap();
+  await OrganizeStrategy.processTracks();
 };
 
 initScript();
